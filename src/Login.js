4e493ac845aeba97a1f55aeaf163bc14a9a3b292
Jubal Jacob <71512643+jubaljacob@users.noreply.github.com>
2021-08-02 21:31:40 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -6,7 +6,7 @@ function login() {
         <div className="login">
             <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
             
-            <button>LOGIN TO SPOTIFY</button>
+            <a>LOGIN WITH SPOTIFY</a>
         </div>
     );
 }
